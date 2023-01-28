d61c200c7dff4853d5f97596c66f6c2a327d6e06
Victor Podin <podin_victor@mail.ru>
2023-01-28 13:32:03 +0300

    finished

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Инициализируем и вводим массив через пробел
 Console.WriteLine("Введите элементы массива через пробел");
-string[] words = Console.ReadLine().Split(' ');
+string[] words = Console.ReadLine().Split(',');
 // Считаем количество элементов в массиве, удовлетворяющих условию задачи, чтобы в новом массиве выделить только нужную память
 int countNewElements = 0;
 // Также, если пользователь случайно нажмет два пробела при вводе, получим пустые элементы. Используем условие word != string.Empty
@@ -8,7 +8,8 @@ foreach (string word in words)
 {
     if (word.Length < 4 && word != string.Empty) countNewElements++;
 }
-// Делаем новый массив известной длины
+if (countNewElements == 0) Console.WriteLine(); // Если не введено элементов удовлетворяющих условию задачи
+// Делаем новый массив уже известной длины
 string[] newwords = new string[countNewElements];
 int i = 0;
 // И заполняем его новыми элементами по условию задачи
@@ -16,7 +17,8 @@ foreach (string word in words)
 {
     if (word.Length < 4 && word != string.Empty) { newwords[i] = word; i++; }
 }
-foreach (string newword in newwords)
+for (i = 0; i < countNewElements; i++)
 {
-    Console.WriteLine(newword);
+    if (i < countNewElements - 1) Console.Write(newwords[i] + ",");
+    if (i == countNewElements - 1) Console.Write(newwords[i]);
 }
