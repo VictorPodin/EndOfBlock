6edc6d1b1d24e88cf2ecbb502d6f9c16456337d1
Victor Podin <podin_victor@mail.ru>
2023-01-28 12:29:17 +0300

    input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Инициализируем и вводим массив через пробел
+Console.WriteLine("Введите элементы массива через пробел");
+string[] words = Console.ReadLine().Split(' ');
+foreach (string word in words)
+{
+    Console.WriteLine(word + " ");
+}
+
